7744f4a3279c604df120c7cc88f6d0a6e6da53b6
mohanraj-exe <smohan1029@gmail.com>
2025-02-07 00:21:28 +0530

    answer_validation_fix

--- routes.js
+++ routes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 router.get('/fetch', async (req, res) => {
     try {
         const unscrambledWords = await UnscrambledWord.find().lean();
-        // console.log(unscrambledWords.length);
         return res.status(200).json({ data: unscrambledWords });
     } catch (error) {
         // console.error(error);
@@ -17,18 +16,22 @@ router.get('/fetch', async (req, res) => {
 
 // Verify the submitted unscrambled word/sentence
 router.post('/verify', async (req, res) => {
-    // console.log();
     try {
         const { selected_id, score, completionTime, typedAnswer } = req.body;
         const word = await UnscrambledWord.findOne({ _id: selected_id }).lean();
-        let actualAnswerToTest = typedAnswer.toLowerCase();
-        if (word.answer !== actualAnswerToTest) {
+        
+        // Converting both actual and typed answer with to lowercase,
+        // avoid white spaces in the start and end and ensuring single spaces between words
+        const normalizeText = (text) => text.toLowerCase().trim().replace(/\s+/g, ' ');
+        const actualAnswer = normalizeText(word.answer);
+        const userAnswer = normalizeText(typedAnswer);
+        if (actualAnswer !== userAnswer) {
             return res.status(400).json({ message: "Words/sentence not matching" });
         }
         const storeResults = await ChallengeResult.create({
             score: score,
             time: completionTime,
-            answer: actualAnswerToTest,
+            answer: userAnswer,
             ansRef: selected_id
         });
         return res.status(200).json({ message: "Success", data: storeResults });
